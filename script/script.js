5ec8b18e26f1e6c40f258de08e36e60bcfc08f06
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-11-02 10:43:18 -0500

    carrito

--- script/script.js
+++ script/script.js
@@ -1,8 +1,35 @@
 const url = ' http://localhost:4000/articulos'
+
 const getData = async(url)=>{
 const resp = await fetch (url);
 const data = await resp.json()
-console.log(data);
-
+const {nombre,imagenP,imagen1, imagen2, descripcion,precio,talla, id} = data
 }
-console.log(getData(url));
+const agregarCarrito = document.getElementById('agregarCarrito');
+agregarCarrito.addEventListener('click', ()=>{
+let urlCar = 'http://localhost:4000/articulos'     
+    await fetch(urlCar, {
+        method: "POST",
+        body: JSON.stringify({
+            nombre,
+            imagenP,
+            descripcion,
+            precio,
+            talla
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+    });
+    console.log("agregaste al carrito")
+})
+
+
+
+const carrito = document.getElementById('carrito');
+carrito.addEventListener('click', ()=>{
+console.log("ingresaste al carrito")
+})
+
+
+
