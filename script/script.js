84cf738f79cc8e3f2db3d45c57639dc29030b53d
EdithRincon <andre-lin@hotmail.es>
2021-11-02 09:46:16 -0500

    cambio en javascript

--- script/script.js
+++ script/script.js
@@ -1,8 +1,20 @@
+let productos = document.getElementById('productos')
+
+productos.addEventListener('click', async () => {
+    producto.innerHTML = ''
 const url = ' http://localhost:4000/articulos'
 const getData = async(url)=>{
 const resp = await fetch (url);
 const data = await resp.json()
 console.log(data);
-
-}
-console.log(getData(url));
+data.forEach(art => {
+    const { nombre, imagenP, id } = art;
+    producto.innerHTML += `<div class="" style="width: XXrem;" id="cardProducto">
+                           <img src="${imagenP}" class="" alt="...">
+                           <div class="">
+                           <h5 class=""><strong>${nombre}</strong></h5>
+                           <a href="#" onClick="(${id})" class="">BUY IT</a>
+                           </div>
+                           `
+})}
+})
