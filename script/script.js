f59da5841a69b4cc4a6f7a3f823c3df97313182f
EdithRincon <andre-lin@hotmail.es>
2021-11-02 10:35:57 -0500

    mostrador de coleccion

--- script/script.js
+++ script/script.js
@@ -1,20 +1,21 @@
 let productos = document.getElementById('productos')
-
+let mostrador=document.getElementById('mostrador')
+let url = ' http://localhost:4019/articulos';
 productos.addEventListener('click', async () => {
-    producto.innerHTML = ''
-const url = ' http://localhost:4000/articulos'
-const getData = async(url)=>{
-const resp = await fetch (url);
-const data = await resp.json()
+    console.log("hola")
+    mostrador.innerHTML = ''
+let resp = await fetch(url);
+let data = await resp.json();
 console.log(data);
 data.forEach(art => {
-    const { nombre, imagenP, id } = art;
-    producto.innerHTML += `<div class="" style="width: XXrem;" id="cardProducto">
+    const { nombre, imagenP, id, precio } = art;
+    mostrador.innerHTML += `<div class="container card" style="width: 14rem;" id="cardproducto">
                            <img src="${imagenP}" class="" alt="...">
-                           <div class="">
-                           <h5 class=""><strong>${nombre}</strong></h5>
+                           <div class="card-body">
+                           <h5 class="card-title d-flex justify-content-center"><strong>${nombre}</strong></h5>
+                           <h5 class="card-title d-flex justify-content-center"><strong>${precio}</strong></h5>
                            <a href="#" onClick="(${id})" class="">BUY IT</a>
                            </div>
                            `
-})}
+})
 })
